51a96de6886835ab9641f2459c85b0b270d1e407
viswans2132 <viswans2132@gmail.com>
2026-02-10 14:25:58 +0100

    Quadtree related files added.

--- _main.cpp
+++ _main.cpp
@@ -1,113 +1,138 @@
-/*
- * main.cpp
- *
- *  Created on: Jun 12, 2010
- *      Author: Kocher
- */
+// /*
+//  * main.cpp
+//  *
+//  *  Created on: Jun 12, 2010
+//  *      Author: Kocher
+//  */
+
+// #include <iostream>
+// #include <cmath>
+// #include <vector>
+
+// #include "utilities/PrintContent.h"
+// #include "utilities/utilities.h"
+
+// void
+// testPoint();
+
+// void
+// testBeamlet();
+
+// void
+// testCluster();
+
+// void
+// testToBase();
+
+
+
+// int main()
+// {
+// 	//std::cout << "Hello world!" << std::endl;
+
+
+// 	//testPoint();
+
+// 	//testBeamlet();
+
+// 	testCluster();
+
+// 	std::string type("X");
+
+// 	size_t found;
+
+// 	found = type.find("X");
+// 	std::vector<int> fctIndex;
+
+// 	while (found!=std::string::npos)
+// 	{
+// 		fctIndex.push_back(found);
+// 		found = type.find("X", found+1);
+// 	}
+
+// 	std::cout << fctIndex << std::endl;
+
+
+// 	/*
+// 	int startArray[] = {1, 1, 1}, goalArray[] = {4, 4, 1} ;
+
+// 	std::vector<int> start(startArray, startArray + sizeof(startArray)/(sizeof(int))),
+// 					 goal(goalArray, goalArray + sizeof(goalArray)/(sizeof(int)));;
+
+
+// 	int n = goal.size();
+// 	std::vector<int> state = start;
+// 	int i = n-1;
+
+// 	while (state[0] <= goal[0])
+// 	{
+
+// 		std::cout << state << std::endl;
+
+// 		state[i] = state[i] + 1;
+
+// 		while(state[i] > goal[i])
+// 		{
+// 			if (i == 0)
+// 				break;
+
+// 			state[i] = start[i];
+// 			i = i - 1;
+// 			state[i] = state[i] + 1;
+// 		}
+
+// 		i = n-1;
+// 	}
+// 	*/
+
+// 	//testToBase();
+
+// 	char buffer[32];
+
+// 	toBase( 9, buffer, 2, 10 );
+
+// 	int coorArray [] = {1, 2, 3};
+
+// 	std::vector<int> coor (coorArray, coorArray + sizeof(coorArray)/sizeof(int) );
+
+
+// 	int dim = coor.size();
+// 	int index = 0;
+// 	int maxSize = 7;
+
+// 	for (int i = 0; i < dim; ++i)
+// 		index = index + coor[i]*pow(maxSize, dim - 1 -i);
+
+// 	std::cout << index << std::endl;
+
+
+
+
+// 	return 0;
+// }
+
 
 #include <iostream>
-#include <cmath>
-#include <vector>
+#include "QuadTreeBuilder.h"
+#include "QuadTreeHelper.h"
+#include "config.h"
 
-#include "utilities/PrintContent.h"
-#include "utilities/utilities.h"
+using namespace std;
 
-void
-testPoint();
+int main() {
+    // Create the QuadTree object
+    QuadTree qt;
 
-void
-testBeamlet();
+    // Build the quadtree starting from scale smax and top-left (1,1)
+    QuadTreeNode* root = qt.buildQuadTree(Config::smax, 1, 1);
 
-void
-testCluster();
+    cout << "\n===== QUADTREE STRUCTURE =====\n\n";
 
-void
-testToBase();
+    // Create a helper object to print the quadtree
+    QuadTreeHelper helper;
+    // helper.printQuadTree(root, 0);
+	helper.printFormalDyadicForm(root);
 
-
-
-int main()
-{
-	//std::cout << "Hello world!" << std::endl;
-
-
-	//testPoint();
-
-	//testBeamlet();
-
-	testCluster();
-
-	std::string type("X");
-
-	size_t found;
-
-	found = type.find("X");
-	std::vector<int> fctIndex;
-
-	while (found!=std::string::npos)
-	{
-		fctIndex.push_back(found);
-		found = type.find("X", found+1);
-	}
-
-	std::cout << fctIndex << std::endl;
-
-
-	/*
-	int startArray[] = {1, 1, 1}, goalArray[] = {4, 4, 1} ;
-
-	std::vector<int> start(startArray, startArray + sizeof(startArray)/(sizeof(int))),
-					 goal(goalArray, goalArray + sizeof(goalArray)/(sizeof(int)));;
-
-
-	int n = goal.size();
-	std::vector<int> state = start;
-	int i = n-1;
-
-	while (state[0] <= goal[0])
-	{
-
-		std::cout << state << std::endl;
-
-		state[i] = state[i] + 1;
-
-		while(state[i] > goal[i])
-		{
-			if (i == 0)
-				break;
-
-			state[i] = start[i];
-			i = i - 1;
-			state[i] = state[i] + 1;
-		}
-
-		i = n-1;
-	}
-	*/
-
-	//testToBase();
-
-	char buffer[32];
-
-	toBase( 9, buffer, 2, 10 );
-
-	int coorArray [] = {1, 2, 3};
-
-	std::vector<int> coor (coorArray, coorArray + sizeof(coorArray)/sizeof(int) );
-
-
-	int dim = coor.size();
-	int index = 0;
-	int maxSize = 7;
-
-	for (int i = 0; i < dim; ++i)
-		index = index + coor[i]*pow(maxSize, dim - 1 -i);
-
-	std::cout << index << std::endl;
-
-
-
-
-	return 0;
+    return 0;
 }
 
